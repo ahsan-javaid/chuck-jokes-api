ab648f5131212649629884e73ace4c4f719715ab
ähsan javaid <hsan786cs@gmail.com>
2021-03-01 01:14:36 +0500

    Add app.js and configurations

---
+++ README.md
@@ -0,0 +1,11 @@
+# Chuck Jokes API
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.
+Open [http://localhost:8000](http://localhost:8000) to view it in the browser.
+
---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
+const app = express();
+app.use(cors())
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true }))
+app.use(bodyParser.json({limit: '50mb'}));
+
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+mongoose.connect(process.db, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Successfully connected to the database");
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+
+require('./routes')(app);
+global.models = require('./models/index');
+
+
+app.listen(process.env.PORT || 8000, () => {
+    console.log("Server running");
+});
+
+
+
